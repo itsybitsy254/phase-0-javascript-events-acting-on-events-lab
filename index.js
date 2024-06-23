4a9298830952575d98d26bb5ccb099afae93cfeb
Vitalis Ian <viterlis254@gmail.com>
2024-06-23 12:24:06 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Your code here
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'ArrowLeft') {
+      moveDodgerLeft();
+    } else if (event.key === 'ArrowRight') {
+      moveDodgerRight();
+    }
+  });
+
+  function moveDodgerRight() {
+    const dodger = document.getElementById('dodger');
+    let left = parseInt(dodger.style.left.replace('px', ''), 10);
+    if (left < 360 - 40) { 
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+  function moveDodgerLeft() {
+    const dodger = document.getElementById('dodger');
+    let left = parseInt(dodger.style.left.replace('px', ''), 10);
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  
+  
+  
